f6c0a8a7649d32cb243827c92db5084865507158
data-pup <kevinmartinucr@gmail.com>
2018-07-17 14:12:04 -0400

    Added a gitignore and some other work.

--- path-sum-ii-rs/src/lib.rs
+++ path-sum-ii-rs/src/lib.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+
+type NodeId = u32;
+
+struct Node<T> {
+    value: T,
+    id: NodeId,
+}
+
+struct BTree<T> {
+    nodes: HashMap<NodeId, Node<T>>,
+    edges: HashMap<NodeId, (NodeId, NodeId)>,
+    root: Option<NodeId>,
+}
+
+impl<T> BTree<T> {
+    fn new() -> Self {
+        Self {
+            nodes: HashMap::new(),
+            edges: HashMap::new(),
+            root: None,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
